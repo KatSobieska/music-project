5a5cbcdfc4dd33f36aa2061062d2c964d472ac39
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-03-29 20:23:02 +0200

    Add const settings

---
+++ src/js/settings.js
@@ -0,0 +1,9 @@
+export const settings = {
+  db: {
+    url:
+      '//' +
+      window.location.hostname +
+      (window.location.hostname == 'localhost' ? ':3131' : ''),
+    songs: 'songs',
+  },
+};
