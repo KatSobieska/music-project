c54f8a1b2999484ef71d533da6550a44200a35c6
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-04-02 20:28:21 +0200

    Add class Home

---
+++ src/js/components/Home.js
@@ -0,0 +1,55 @@
+import utils from '../utils.js';
+import { select, templates, classNames } from '../settings.js';
+
+class Home {
+  constructor(element) {
+    const thisHome = this;
+
+    thisHome.render(element);
+  }
+
+  render(element) {
+    const thisHome = this;
+
+    const generatedHTML = templates.home();
+
+    thisHome.element = utils.createDOMFromHTML(generatedHTML);
+
+    const homeContainer = document.querySelector(select.containerOf.home);
+
+    homeContainer.appendChild(thisHome.element).innerHTML;
+
+    thisHome.dom = {};
+    thisHome.dom.wrapper = element;
+
+    thisHome.pages = document.querySelector(select.containerOf.pages).children;
+    thisHome.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisHome.pages[0].id;
+
+    for (let page of thisHome.pages) {
+      if (page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        console.log(pageMatchingHash);
+        break;
+      }
+    }
+    thisHome.activatePage(pageMatchingHash);
+  }
+  activatePage(pageId) {
+    const thisHome = this;
+
+    for (let page of thisHome.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    for (let link of thisHome.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  }
+}
+export default Home;
