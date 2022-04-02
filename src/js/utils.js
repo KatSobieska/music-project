81cc96a5c77cc1362f470f5e5a7885197cebf56f
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-04-02 20:28:36 +0200

    Add utils

---
+++ src/js/utils.js
@@ -0,0 +1,28 @@
+/* global Handlebars, dataSource */
+
+const utils = {}; // eslint-disable-line no-unused-vars
+
+utils.createDOMFromHTML = function (htmlString) {
+  let div = document.createElement('div');
+  div.innerHTML = htmlString.trim();
+  return div.firstChild;
+};
+
+utils.convertDataSourceToDbJson = function () {
+  const productJson = [];
+  for (let key in dataSource.products) {
+    productJson.push(Object.assign({ id: key }, dataSource.products[key]));
+  }
+
+  console.log(JSON.stringify({ product: productJson, order: [] }, null, '  '));
+};
+
+Handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
+  return arg1 == arg2 ? options.fn(this) : options.inverse(this);
+});
+
+Handlebars.registerHelper('joinValues', function (input, options) {
+  return Object.values(input).join(options.fn(this));
+});
+
+export default utils;
