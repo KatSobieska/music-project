f372d79b5264de8c63f6590c748c850a0f5f71a3
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-04-04 19:29:34 +0200

    Add const select, classNames, templates

--- src/js/settings.js
+++ src/js/settings.js
@@ -7,3 +7,41 @@ export const settings = {
     songs: 'songs',
   },
 };
+
+export const select = {
+  containerOf: {
+    pages: '#pages',
+    home: '.home-wrapper',
+    search: '.search-wrapper',
+    discover: '.discover-wrapper',
+  },
+  templateOf: {
+    home: '#template-home-page',
+    search: '#template-search-page',
+    discover: '#template-discover-page',
+  },
+  nav: {
+    links: '.main-nav a',
+  },
+};
+
+export const classNames = {
+  pages: {
+    active: 'active',
+  },
+  nav: {
+    active: 'active',
+  },
+};
+
+export const templates = {
+  search: Handlebars.compile(
+    document.querySelector(select.templateOf.search).innerHTML
+  ),
+  discover: Handlebars.compile(
+    document.querySelector(select.templateOf.discover).innerHTML
+  ),
+  home: Handlebars.compile(
+    document.querySelector(select.templateOf.home).innerHTML
+  ),
+};
